471c9c42c9be31279a3812b7fe4bd2a399761903
hheister <horst.heistermann@oracle.com>
2015-06-01 16:32:24 -0700

    updated db connection string

--- local.config.js
+++ local.config.js
@@ -2,7 +2,7 @@
 
 exports.config = {
     //db_uri: 'mongodb://localhost:27017/SampleApp',
-    db_uri: 'mongodb://heroku_app37413426:c2qshsi9d8lrv0p9usgod8amom@ds043022.mongolab.com:43022/heroku_app37413426',
+    db_uri: 'mongodb://heroku_app37418932:heroku_app37418932@ds043082.mongolab.com:43082/heroku_app37418932',
     db_config: {
         host: "localhost",
         // use default "port"
